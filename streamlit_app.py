e8789386415d40762a73163084680e929725167e
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2023-12-18 22:16:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,10 +131,10 @@ with st.form("my_form"):
     def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
         return df.to_csv().encode('utf-8')
-   csv = convert_df(eksponerte_bygg_syk)
-   st.download_button(
+    csv = convert_df(eksponerte_bygg_syk)
+    st.download_button(
        label="Download data as CSV",
        data=csv,
        file_name='large_df.csv',
        mime='text/csv',
-   )
+       )
