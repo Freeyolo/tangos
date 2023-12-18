2f2cfecc7796f03aaeeb62e3788a0305371ad154
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2023-12-18 22:11:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,5 +127,17 @@ with st.form("my_form"):
     # =============================================================================
     # Eksportering av data i CSV format
     # =============================================================================
-    utbane = os.path.join(cwd, r'eksponerte_bygg_qd_syk.csv')
-    eksponerte_bygg_syk.to_csv(utbane)
+
+    @st.cache
+    def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+   csv = convert_df(eksponerte_bygg_syk)
+
+   st.download_button(
+       label="Download data as CSV",
+       data=csv,
+       file_name='large_df.csv',
+       mime='text/csv',
+   )
