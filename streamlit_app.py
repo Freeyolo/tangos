73b91bd9644d69ccaab5c9fbc42ad3a69a81c874
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2023-12-19 08:40:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,6 +119,7 @@ with st.form("my_form"):
     output['QD_bolig'] = QD_bolig
     output['QD_vei'] = QD_vei
     output_csv = pd.DataFrame(output) #konverter tilbake til pandas dataframe
+    bygningstype = pd.read_csv(bygningstype.csv)
    
     # =============================================================================
     # Plotting av data i kart og lagring av kartet
