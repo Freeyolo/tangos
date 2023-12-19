b805a122cf026ee745209599395be2f5405ca693
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2023-12-19 09:23:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,7 @@ with st.form("my_form"):
     result_geodataframe = pd.concat([get_geo_data(row) for index, row in gdf_syk_bbox.iterrows()], ignore_index=True)
     eksponerte_bygg_syk = gpd.sjoin(result_geodataframe,gdf_syk,predicate='within') # finner bygninger fra matrikkelen innenfor sikkerhetsavstanden
     output = eksponerte_bygg_syk.copy()
-    output = output.drop(columns=['gml_id', 'oppdateringsdato', 'stedfestingVerifisert', 'bygningsnummer','opprinnelse', 'uuidBruksenhet', 'uuidBygning', 'bygningId', 'navnerom', 'versjonId','index_right'])
+    output = output.drop(columns=['gml_id', 'oppdateringsdato', 'stedfestingVerifisert', 'bygningsnummer','opprinnelse', 'uuidBruksenhet', 'uuidBygning', 'bygningId', 'navnerom','bruksenhetId','adresseId','matrikkelenhetId', 'versjonId','index_right'])
     output['QD_bolig'] = QD_bolig
     output['QD_vei'] = QD_vei
     output_csv = pd.DataFrame(output) #konverter tilbake til pandas dataframe
