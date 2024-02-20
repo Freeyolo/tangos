fc5d582d9509b2ca0c9a6c21b0af0bb057aa601a
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2024-02-20 20:46:34 +0100

    test mindre kolonner

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,8 @@ with st.form("my_form"):
         
     result_geodataframe = pd.concat([get_geo_data(row) for index, row in gdf_syk_bbox.iterrows()], ignore_index=True)
     eksponerte_bygg_syk = gpd.sjoin(result_geodataframe,gdf_syk,predicate='within') # finner bygninger fra matrikkelen innenfor sikkerhetsavstanden
-    output = eksponerte_bygg_syk.copy()
+    output = eksponerte_bygg_syk[['bygningstype']]
+    #output = eksponerte_bygg_syk.copy()
     output['QD_bolig'] = QD_bolig
     output['QD_vei'] = QD_vei
     output_csv = pd.DataFrame(output) #konverter tilbake til pandas dataframe
