f0b6983673d2e7dac708eaacfbda13efe531e9b4
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2024-02-27 19:23:22 +0100

    width modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -223,7 +223,7 @@ with st.form("my_form"):
         kartQDvei = gdf_vei.explore(m=kartpunkt,style_kwds=dict(fill=False,color='yellow'))
         kart2 = output.explore(m=kartpunkt,style_kwds=dict(color='red'),name ="Bygninger")
         folium.LayerControl().add_to(kart2)
-        st_kart = st_folium(kart2,width=700,zoom=13)
+        st_kart = st_folium(kart2,width=900,zoom=13)
           
     else:
         output_csv = pd.DataFrame()
@@ -234,7 +234,7 @@ with st.form("my_form"):
         kartQDsyk = gdf_syk.explore(m=kartpunkt,style_kwds=dict(fill=False,color='red'))
         kartQDbol = gdf_bolig.explore(m=kartpunkt,style_kwds=dict(fill=False,color='orange'))
         kartQDvei = gdf_vei.explore(m=kartpunkt,style_kwds=dict(fill=False,color='yellow'))
-        st_kart = st_folium(kartpunkt,width=700,zoom=13)
+        st_kart = st_folium(kartpunkt,width=900,zoom=13)
  
 # =============================================================================
 # Eksportering av data i CSV format
