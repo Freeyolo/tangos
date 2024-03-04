80a40e49c2c6b0a3fb9c94aa62236412406265b7
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2024-03-04 21:11:35 +0100

    avrunding

--- streamlit_app.py
+++ streamlit_app.py
@@ -243,7 +243,7 @@ with st.form("my_form"):
         output.drop(columns=['Kodeverdi'], inplace=True) #fjern unødvendig kolonne
 
         output['avstand'] = round(output.distance(gdf.iloc[0]['geometry'])) #regn ut avstanden til eksplosivlageret
-        output['trykk'] = output['avstand'].apply(incident_pressure) #regner ut trykket
+        output['trykk'] = output['avstand'].apply(incident_pressure).round(2) #regner ut trykket og runder av til to desimaler
 
         output['bygningstype'] = output['bygningstype'].astype(str) # Convert 'bygningstype' column to string type
         boliger = output[output['bygningstype'].str.startswith('1')]
