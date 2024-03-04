b02b1fa5e26691a621419eaf321e15f0d6147217
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2024-03-04 20:59:44 +0100

    rounding and cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,10 +236,13 @@ with st.form("my_form"):
     if not result_geodataframe.empty:
         eksponerte_bygg_syk = gpd.sjoin(result_geodataframe, gdf_syk, predicate='within') #behold bare bygninger innenfor sirkelen
         output = eksponerte_bygg_syk[['bygningstype', 'geometry']] #kun interessant med disse kolonnene for videre visualisering
+        
         bygningstype = pd.read_csv(bygningstype_url, index_col=False, sep=';', usecols=['Navn', 'Kodeverdi'], encoding='utf8') #last in bygningstype fra SSB
         output = output.merge(bygningstype, how='left', left_on='bygningstype', right_on='Kodeverdi') #få på leselige navn på bygningstype
         output.drop(columns=['Kodeverdi'], inplace=True) #fjern unødvendig kolonne
-        output['avstand'] = output.distance(gdf.iloc[0]['geometry']) #regn ut avstanden til eksplosivlageret
+
+        output['avstand'] = round(output.distance(gdf.iloc[0]['geometry'])) #regn ut avstanden til eksplosivlageret
+        
         output['bygningstype'] = output['bygningstype'].astype(str) # Convert 'bygningstype' column to string type
         boliger = output[output['bygningstype'].str.startswith('1')]
         industri = output[output['bygningstype'].str.startswith('2')]
