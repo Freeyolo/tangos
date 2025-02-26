276e38de863c5cd38efcd19b1db2c0c581d2dc46
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2025-02-26 13:57:58 +0100

    Oppdatert rett trykk ved avstand offentlig vei fra 14 kPa til 9 kPa

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,7 +214,7 @@ with st.form("my_form"):
     gdf_bolig['trykk'] = '5 kPa'
 
     gdf_vei['QD_vei'] = QD_vei
-    gdf_vei['trykk'] = '14 kPa'
+    gdf_vei['trykk'] = '9 kPa'
     
     gdf_syk['geometry'] = gdf_syk['geometry'].buffer(gdf_syk['QD_syk'])  
     gdf_bolig['geometry'] = gdf_bolig['geometry'].buffer(gdf_bolig['QD_bolig'])
